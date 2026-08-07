43bab4110fa7d51e63234daed0ac3de24f4e3000
aaagrud <durgasumesh17@gmail.com>
2026-08-07 08:18:34 +0530

    add unit tests for block_helpers

--- src/helpers/block_conversion_helpers.py
+++ src/helpers/block_conversion_helpers.py
@@ -8,19 +8,19 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     return blocks_stripped_no_empty
 
 def block_to_blocktype(block: str) -> BlockType:
-    regex_heading = r"#{1,6}"
-    regex_code = r"```\n(.*)```"
+    regex_heading = r"#{1,6} (.*|\n)"
+    regex_code = r"```\n[\s\S]*?```"
     regex_quote = r"(>.*(\r?\n|$))+"
     regex_unordered_list = r"(- .*(\r?\n|$))+"
     regex_ordered_list = r"(\d\. .*(\r?\n|$))+"
-    if re.match(regex_heading, block):
+    if re.fullmatch(regex_heading, block):
         return BlockType.HEADING
-    if re.match(regex_code, block):
+    if re.fullmatch(regex_code, block):
         return BlockType.CODE
-    if re.match(regex_quote, block):
+    if re.fullmatch(regex_quote, block):
         return BlockType.QUOTE
-    if re.match(regex_unordered_list, block):
+    if re.fullmatch(regex_unordered_list, block):
         return BlockType.UNORDERED_LIST
-    if re.match(regex_ordered_list, block):
+    if re.fullmatch(regex_ordered_list, block):
         return BlockType.ORDERED_LIST
     return BlockType.PARGRAPH
--- src/tests/test_block_helpers.py
+++ src/tests/test_block_helpers.py
@@ -1,6 +1,6 @@
 import unittest
 import src.helpers.block_conversion_helpers as block_conversion_helpers
-
+from src.constants import BlockType
 class TestBlockHelpers(unittest.TestCase):
     def test_markdown_to_blocks(self):
         md = """
@@ -45,3 +45,45 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_paragraph(self):
+        md = """
+"How are you?" is a very common greeting in english. 
+
+"How do you do?" is an old fashioned way of saying the same thing!
+"""
+        block_type = block_conversion_helpers.block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.PARGRAPH)
+
+    def test_block_to_block_type_heading(self):
+        md = "# 'How are you?' is a very common greeting in english."
+        block_type = block_conversion_helpers.block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_block_type_code(self):
+        md = '''```
+for i in range(10):
+    print(i)```'''
+        block_type = block_conversion_helpers.block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        md = '''> oink
+>yoink'''
+        block_type = block_conversion_helpers.block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_block_type_unordered_list(self):
+        md = '''- oink
+- yoink'''
+        block_type = block_conversion_helpers.block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_ordered_list(self):
+        md = '''1. oink
+2. yoink'''
+        block_type = block_conversion_helpers.block_to_blocktype(md)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+
+   
