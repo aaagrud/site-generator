84181a310be653bce6c9421eb2f39b38e6c86f72
aaagrud <durgasumesh17@gmail.com>
2026-08-13 08:55:14 +0530

    fix bug links, css not working

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from helpers.file_helpers import copy_content_static_to_public_recursive
 from helpers.website_generation_helpers import generate_pages_recursive
 
 def main(args):
-    base_path = args[0] if args else '/'
+    base_path = args[1] if args else '/'
     copy_content_static_to_public_recursive('./static', './docs')
     generate_pages_recursive('./content', './template.html', './docs', base_path)
 
